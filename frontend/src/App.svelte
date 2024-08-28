c0ad82c413c9121bccc941088b316fd33dd5d554
piu0887 <147304229+piu0887@users.noreply.github.com>
2024-08-29 01:04:45 +0900

    first commit

---
+++ frontend/src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  import Router from 'svelte-spa-router'
+  import Home from "./routes/Home.svelte"
+  import Navigation from "./components/Navigation.svelte"
+  import UserCreate from "./routes/UserCreate.svelte"
+  import UserLogin from "./routes/UserLogin.svelte"
+
+
+
+  const routes = {
+    '/': Home,
+    '/user-create': UserCreate,
+    '/user-login': UserLogin,
+
+  }
+</script>
+
+<Navigation />
+<Router {routes}/>
+
---
+++ frontend/src/routes/home.svelte
@@ -0,0 +1,11 @@
+<script>
+    import fastapi from "../lib/api"
+    import { link } from 'svelte-spa-router'
+    import { page } from "../lib/store"
+    import moment from 'moment/min/moment-with-locales'
+    moment.locale('ko')
+
+   
+</script>
+
+<h1>로그인/회원가입 페이지</h1>
